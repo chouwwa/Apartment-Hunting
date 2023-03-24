c59db26e1ad6c102be5200b155f6c5d777eb5f92
wc <waylchan@cisco>
2023-03-23 17:55:05 -0700

    Changed to black and created some helper functions

--- src/pipelines/apartments/scraping_solids.py
+++ src/pipelines/apartments/scraping_solids.py
@@ -1,4 +1,5 @@
-import requests
+# import requests
+from requests_html import HTMLSession, HTMLResponse
 import re
 import datetime
 from bs4 import BeautifulSoup
@@ -28,6 +29,7 @@ from dagster import (
     # EventMetadataEntry,
     Output,
 )
+
 # from .types_realestate import PropertyDataFrame, SearchCoordinate, JsonType
 
 
@@ -37,27 +39,62 @@ from dagster_aws.s3.solids import S3Coordinate
 # from realestate.common.solids_filehandle import json_to_gzip
 # from dagster.utils.temp_file import get_temp_file_name
 
+
 def apartments_region_search(area: String, max_n=100):
     # only USA
     # city, state
-    usa = area.split(',')
+    usa = area.split(",")
     city = usa[0].strip().lower()
     state = usa[1].strip().lower()
 
-    base_url = 'https://www.apartments.com/'
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-               }
-    page = requests.get(base_url + city + '-' + state, headers=headers)
+    base_url = "https://www.apartments.com/"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+    }
 
-    bs = BeautifulSoup(page.content, 'html.parser')
-    listings_find = bs.find_all('div', class_='content-wrapper')
-    listings = {}
-    for each in listings_find:
-        listings[each.find('a').get('aria-label').split(',')[0]] = each.text
+    # Start session and get HTMLResponse
+    page = get_url(base_url + city + "-" + state, headers=headers)
+    # page = get_url(
+    #     "https://webscraper.io/test-sites/e-commerce/allinone/phones", headers=headers
+    # )
 
-    with open('./test_scrape.json', 'w') as f:
-        json.dump(listings, f)
-    
+    # Get list of items with css
+    # listings = page.html.xpath("/html/body/div[1]/div[3]/div/div[2]/div/div/div/div[1]")
+    listings = page.html.xpath(
+        '//*[@id="placardContainer"]/ul/li/article/section/div/div[2]/div'
+    )
+
+    print(listings)
+
+    save_html(page, "test_scrape.pkl")
 
 
-apartments_region_search('PleaSaNton, cA')
+def get_url(url, headers) -> HTMLResponse:
+    session = HTMLSession()
+    return session.response_hook(session.get(url, headers=headers))
+
+
+def xpath_find(page, path):
+    return page.html.xpath(path)
+
+
+def save_html(page, fn):
+    with open("./{fn}".format(fn=fn), "wb") as f:
+        pickle.dump(page, f)
+
+
+def load_html(fn):
+    with open("./{fn}".format(fn=fn), "rb") as f:
+        return pickle.load(f)
+
+
+# apartments_region_search("PleaSaNton, cA")
+
+session = HTMLSession()
+page = session.response_hook(load_html("test_scrape.pkl"))
+
+print(
+    page.html.xpath(
+        '//*[@id="placardContainer"]/ul/li/article/section/div/div[2]/div[1]/a'
+    )
+)
