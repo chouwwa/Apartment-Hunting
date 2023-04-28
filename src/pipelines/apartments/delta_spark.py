b3d8595473bd1a1a4bdcec32831594fee17eda4b
wc <waylchan@cisco>
2023-04-28 15:51:45 -0700

    initial apache spark

---
+++ src/pipelines/apartments/delta_spark.py
@@ -0,0 +1,15 @@
+from delta import *
+import pyspark
+
+builder = (
+    pyspark.sql.SparkSession.builder.appName("apartment_hunting")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+    )
+)
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+# df_spark =
