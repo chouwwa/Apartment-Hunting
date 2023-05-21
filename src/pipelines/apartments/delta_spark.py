2b55980881812764f11d059a972c6d4d8408333c
Waylon Chan <waylon.chan3@gmail.com>
2023-05-21 02:08:50 -0700

    notes on setup

--- src/pipelines/apartments/delta_spark.py
+++ src/pipelines/apartments/delta_spark.py
@@ -44,18 +44,22 @@ def remote_fs(acc_key, sec_key, remote):
 
 builder = local_fs()
 
+# Place winutil and hadoop.dll from github repos of hadoop 3.3.1 bin's into $spark_home$/bin
+# hadoop-aws version can be higher but must be compatible with spark version
 spark = delta.configure_spark_with_delta_pip(
     builder,
     [
         "org.apache.hadoop:hadoop-aws:3.3.2",
-        "com.amazonaws:aws-java-sdk-bundle:1.12.472",
+        # "com.amazonaws:aws-java-sdk-bundle:1.12.472",
     ],
 ).getOrCreate()
-# rdd = spark.sparkContext.parallelize(scraping_solids.get_listings(scraping_solids.page))
 
+# rdd = spark.sparkContext.parallelize(scraping_solids.get_listings(scraping_solids.page))
 # df = rdd.toDF(["names", "prices", "beds", "amenities", "links"])
+
 listings = scraping_solids.listings
 
+# This Schema works if the input is a list of rows in this exact order
 schema = StructType(
     [
         StructField("name", StringType(), True),
@@ -75,3 +79,8 @@ df.printSchema()
 # print(df)
 # print(df.head())
 df.write.format("delta").save("s3a://data/test_scrape")
+
+df = spark.read.load("s3a://data/test_scrape")
+
+df.show()
+df.printSchema()
